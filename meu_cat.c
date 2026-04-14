e111f871bffe07debe9808c41d6cb5fdffd4f967
Theo Mello <theo@theo.theo>
2026-04-14 14:18:03 +0100

    aaaaaaaaa

---
+++ meu_cat.c
@@ -0,0 +1,16 @@
+#include <fcntl.h>
+#include <unistd.h>
+int main()
+{
+	char *arquivo = "teste.txt";
+	char buff[1024];
+	int ru;
+	int bytes;
+	
+	ru = open(arquivo , O_RDONLY);
+	bytes = read(ru, buff , 1024);
+	if (bytes > 0){
+		write(1,buff,bytes);}
+	close(ru);
+	return 0;
+}
